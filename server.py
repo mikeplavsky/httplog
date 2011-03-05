fa5aff7d793272135d3efd1f245484aac6691877
mikeplavsky <Mike.Plavsky@quest.com>
2011-03-05 21:19:18 +0300

    notepad++ breaks returns :(

--- server.py
+++ server.py
@@ -1,25 +1,44 @@
 import cherrypy
-from cherrypy import exposefrom Queue import Queue, Emptydef log(entry):    
+from cherrypy import expose
+from Queue import Queue, Empty
+
+def log(entry):    
     cherrypy.engine.log_cache.put( entry )    
     
 class Root:
     
     @expose
     def log(self,*args,**kwargs):
-            import pickle        record = pickle.loads( kwargs[ 'record' ] )        
-        log( record )                
+    
+        import pickle
+        record = pickle.loads( kwargs[ 'record' ] )
+        
+        log( record )        
+        
         return 'Done'    
         
 if __name__ == '__main__':        
     
     from logging.handlers import RotatingFileHandler
-    handler = RotatingFileHandler('httplogger.txt', maxBytes=2 * 1024, backupCount=5)        def write():
+    handler = RotatingFileHandler('httplogger.txt', maxBytes=2 * 1024, backupCount=5)
+    
+    def write():
         
-        queue = cherrypy.engine.log_cache                    while True:                        try:                                   handler.emit( queue.get_nowait() )                                    except Empty, e:                return               
+        queue = cherrypy.engine.log_cache
+            
+        while True:
+            
+            try:                   
+                handler.emit( queue.get_nowait() )    
+                    
+            except Empty, e:
+                return       
+        
         
     from cherrypy.process import plugins
     
-    cherrypy.engine.log_writer = plugins.Monitor(cherrypy.engine,write,15)        cherrypy.engine.log_writer.subscribe()
+    cherrypy.engine.log_writer = plugins.Monitor(cherrypy.engine,write,15)    
+    cherrypy.engine.log_writer.subscribe()
     
     cherrypy.engine.log_cache = Queue()
     app = cherrypy.quickstart( Root(), config = "config.txt" )
